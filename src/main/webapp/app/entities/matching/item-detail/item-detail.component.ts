0f5813e3e4f0d0391553646bb269051fda3c7443
tienminh <tienminh0628@gmail.com>
2024-04-08 20:51:56 +0700

    spike order page

--- src/main/webapp/app/entities/matching/item-detail/item-detail.component.ts
+++ src/main/webapp/app/entities/matching/item-detail/item-detail.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import ActivateRoute from '../../../account/activate/activate.route';
+import { ActivatedRoute } from '@angular/router';
+import { MatchingService } from '../matching.service';
+import { map, pluck, switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { NzButtonComponent } from 'ng-zorro-antd/button';
+import { NzTabComponent, NzTabSetComponent } from 'ng-zorro-antd/tabs';
 
 @Component({
   selector: 'jhi-item-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    NzButtonComponent,
+    NzTabSetComponent,
+    NzTabComponent
+  ],
   templateUrl: './item-detail.component.html',
   styleUrl: './item-detail.component.scss'
 })
 export class ItemDetailComponent {
-
+  id!: number;
+  data: any;
+  isChecked = false;
+  constructor(private route: ActivatedRoute, private matchingService: MatchingService) {
+    route.params.pipe(pluck('id'),switchMap(id => this.matchingService.getDetailItem(Number(id)))).subscribe(res => {
+      this.data = res;
+    })
+  }
 }
--- src/main/webapp/app/entities/matching/matching.service.ts
+++ src/main/webapp/app/entities/matching/matching.service.ts
@@ -6,28 +6,46 @@ import { ApplicationConfigService } from 'app/core/config/application-config.ser
 
 @Injectable({ providedIn: 'root' })
 export class MatchingService {
+
+  list: any[] = [];
   constructor(
     private http: HttpClient,
     private applicationConfigService: ApplicationConfigService,
-  ) {}
-
-  getProducts(): Observable<{}> {
+  ) {
     const fakeContent = [
       'Emerald and Diamond Ring in 18k White Gold',
       'CARTIER 22 autumn and winter women\'s ring Bague Panthere de Cartier',
       'SalvatoreFerragamo/Ferragamo men\'s leather pin buckle double-sided belt belt 679781',
       'Ebo Explorer Series 19108236'
+    ];
+
+    const fakeDetail = [
+      'From the G-Timeless Collection',
+      'Water resistant to 50 metres',
+      'Round goldtone stainless steel case, 38mm (1.5")',
+      'Goldtone Diamante pattern dial',
+      'Date window at 6 o\'clock',
+      'Goldtone stainless steel bracelet',
+      'Made in Italy'
     ]
-    const list = this.fakeArray(9).map((_, index) => ({
+    this.list = this.fakeArray(9).map((_, index) => ({
       img: `content/images/item${index+1}.jpg`,
-      des: fakeContent[this.randomBetween1And4()],
+      title: fakeContent[this.randomBetween1And4()],
       price: 32423.00,
-      id: index
+      id: index,
+      detail: fakeDetail
     }));
+  }
+
+  getProducts(): Observable<any> {
    /* return this.http.get(this.applicationConfigService.getEndpointFor('api/activate'), {
       params: new HttpParams().set('key', key),
     });*/
-    return of(list);
+    return of(this.list);
+  }
+
+  getDetailItem(id: number): Observable<any> {
+    return of(this.list.find(item => item.id === id));
   }
 
   randomBetween1And4(): number {
@@ -36,4 +54,5 @@ export class MatchingService {
   fakeArray(length: number): any[] {
     return new Array(length).fill(null);
   }
+
 }
