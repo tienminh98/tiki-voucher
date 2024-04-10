d6dd663933fd48cdcdf2d7a9133b2b02c239633a
tienminh <tienminh0628@gmail.com>
2024-04-10 08:49:36 +0700

    update delivery

--- src/main/webapp/app/entities/matching/item-detail/item-detail.component.ts
+++ src/main/webapp/app/entities/matching/item-detail/item-detail.component.ts
@@ -7,6 +7,11 @@ import { Observable } from 'rxjs';
 import { NzButtonComponent } from 'ng-zorro-antd/button';
 import { NzTabComponent, NzTabSetComponent } from 'ng-zorro-antd/tabs';
 import {NzTooltipDirective} from "ng-zorro-antd/tooltip";
+import { NzFormControlComponent, NzFormDirective, NzFormItemComponent, NzFormLabelComponent } from 'ng-zorro-antd/form';
+import { NzColDirective, NzRowDirective } from 'ng-zorro-antd/grid';
+import { FormControl, FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NzInputDirective } from 'ng-zorro-antd/input';
+import { left, right } from '@popperjs/core';
 
 @Component({
   selector: 'jhi-item-detail',
@@ -15,7 +20,15 @@ import {NzTooltipDirective} from "ng-zorro-antd/tooltip";
     NzButtonComponent,
     NzTabSetComponent,
     NzTabComponent,
-    NzTooltipDirective
+    NzTooltipDirective,
+    NzFormDirective,
+    NzFormItemComponent,
+    NzFormLabelComponent,
+    NzFormControlComponent,
+    NzColDirective,
+    ReactiveFormsModule,
+    NzInputDirective,
+    NzRowDirective
   ],
   templateUrl: './item-detail.component.html',
   styleUrl: './item-detail.component.scss'
@@ -24,9 +37,32 @@ export class ItemDetailComponent {
   id!: number;
   data: any;
   isChecked = false;
-  constructor(private route: ActivatedRoute, private matchingService: MatchingService) {
+  deliveryForm!: FormGroup;
+  constructor(private route: ActivatedRoute, private matchingService: MatchingService, private fb: NonNullableFormBuilder) {
     route.params.pipe(pluck('id'),switchMap(id => this.matchingService.getDetailItem(Number(id)))).subscribe(res => {
       this.data = res;
-    })
+    });
+    this.deliveryForm = this.fb.group({
+      realName: ['', [Validators.required]],
+      address: ['', [Validators.required]],
+      phoneNumber: [null, [Validators.required, Validators.pattern(/^\d+$/)]],
+      gmail: ['', [Validators.email, Validators.required]],
+    });
   }
+
+  submitForm(): void {
+    if (this.deliveryForm.valid) {
+      console.log('submit', this.deliveryForm.value);
+    } else {
+      Object.values(this.deliveryForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+
+  protected readonly right = right;
+  protected readonly left = left;
 }
