ebd08a03ed2497bb7bbf724b9b9bfadce632a5b2
tienminh98 <tienminh6898@gmail.com>
2024-06-05 14:17:11 +0700

    update code

--- src/main/webapp/app/entities/withdraw/withdraw.component.ts
+++ src/main/webapp/app/entities/withdraw/withdraw.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import {NzIconDirective} from "ng-zorro-antd/icon";
+import {RouterLink} from "@angular/router";
+import {NzButtonComponent} from "ng-zorro-antd/button";
+import {NzColDirective, NzRowDirective} from "ng-zorro-antd/grid";
+import {NzFormControlComponent, NzFormDirective, NzFormItemComponent} from "ng-zorro-antd/form";
+import {NzInputDirective, NzInputGroupComponent} from "ng-zorro-antd/input";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'jhi-withdraw',
   standalone: true,
-  imports: [],
+  imports: [
+    NzIconDirective,
+    RouterLink,
+    NzButtonComponent,
+    NzColDirective,
+    NzFormControlComponent,
+    NzFormDirective,
+    NzFormItemComponent,
+    NzInputDirective,
+    NzInputGroupComponent,
+    NzRowDirective,
+    ReactiveFormsModule
+  ],
   templateUrl: './withdraw.component.html',
   styleUrl: './withdraw.component.scss'
 })
